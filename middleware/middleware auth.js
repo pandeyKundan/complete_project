568723a98062fc9a7e4987cdaee6a228a258a674
pandeyKundan <Kundankumarraj1317@gmail.com>
2026-04-05 17:18:22 +0530

    Add files via upload

---
+++ middleware/middleware auth.js
@@ -0,0 +1,8 @@
+function isAuthenticated(req, res, next) {
+  if (req.session.userId) {
+    return next();
+  }
+  res.status(401).json({ error: 'Unauthorized' });
+}
+
+module.exports = { isAuthenticated };
