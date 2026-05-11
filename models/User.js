a18adc73cd247db30fbf06722f01e9c2027d7849
pandeyKundan <Kundankumarraj1317@gmail.com>
2026-05-12 01:48:45 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,39 +1,23 @@
-const { getDb } = require('../config/database');
+const { getDb, getQuery, runQuery } = require('../config/database');
 const bcrypt = require('bcrypt');
 
 async function createUser({ email, password, firstName, lastName, company }) {
-  const db = getDb();
   const hashed = await bcrypt.hash(password, 10);
-  return new Promise((resolve, reject) => {
-    db.run(
-      `INSERT INTO users (email, password, first_name, last_name, company) VALUES (?, ?, ?, ?, ?)`,
-      [email, hashed, firstName, lastName, company],
-      function(err) {
-        if (err) reject(err);
-        else resolve({ id: this.lastID, email, firstName, lastName });
-      }
-    );
-  });
+  return runQuery(
+    `INSERT INTO users (email, password, first_name, last_name, company) VALUES (?, ?, ?, ?, ?)`,
+    [email.toLowerCase(), hashed, firstName, lastName, company || null]
+  );
 }
 
 async function findUserByEmail(email) {
-  const db = getDb();
-  return new Promise((resolve, reject) => {
-    db.get(`SELECT * FROM users WHERE email = ?`, [email], (err, row) => {
-      if (err) reject(err);
-      else resolve(row);
-    });
-  });
+  return getQuery(`SELECT * FROM users WHERE email = ?`, [email.toLowerCase()]);
 }
 
 async function findUserById(id) {
-  const db = getDb();
-  return new Promise((resolve, reject) => {
-    db.get(`SELECT id, email, first_name, last_name, company, created_at FROM users WHERE id = ?`, [id], (err, row) => {
-      if (err) reject(err);
-      else resolve(row);
-    });
-  });
+  return getQuery(
+    `SELECT id, email, first_name, last_name, company, created_at FROM users WHERE id = ?`,
+    [id]
+  );
 }
 
 module.exports = { createUser, findUserByEmail, findUserById };
